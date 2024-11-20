f1f8e070ebea232394fa7540479afcea17ee30b3
datht4889 <hoangdat18112003@gmail.com>
2024-11-20 15:49:56 +0700

    Update worker.py

--- utils/worker.py
+++ utils/worker.py
@@ -276,7 +276,8 @@ class Worker(object):
                             new_loss = []
                             alpha = 0.5
                             for _ in loss:
-                                new_loss.append(sum(_ + torch.tensor(loss).to("cuda")*alpha).item())
+                                new_loss.append(sum(_ + torch.tensor(loss).to(self.device)*alpha).item())
+                            new_loss = torch.tensor(new_loss).to(self.device)
                             ##############
 
                             loss, alpha = self.mul_loss(losses=new_loss, shared_parameters=parameters)
