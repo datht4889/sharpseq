d189071187108644d8f2e723e513dd8e0aa84621
datht4889 <hoangdat18112003@gmail.com>
2025-01-13 10:52:26 +0700

    Update weight_methods.py

--- utils/weight_methods.py
+++ utils/weight_methods.py
@@ -476,10 +476,13 @@ class ExcessMTL(WeightMethod):
         for i in range(self.n_tasks):
             self.grad_sum[i] += shared_grads[i] ** 2
             grad_i = shared_grads[i]
-            h_i = torch.sqrt(self.grad_sum[i] + 1e-4)
+            h_i = torch.sqrt(self.grad_sum[i] + 1e-7)
             if h_i.isnan().any():
                 raise ValueError("h_i contains NaN in weighted_methods.py")
             w[i] = grad_i * (1 / h_i) @ grad_i.t()
+            if w[i].isnan().any():
+                raise ValueError("w[i] contains NaN in weighted_methods.py")
+            
 
         if self.first_epoch:
             self.initial_w = w
