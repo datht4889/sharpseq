0d9a405c5a53d9f70491167b7f1d31368d0c1a50
datht4889 <hoangdat18112003@gmail.com>
2025-01-26 09:34:25 +0700

    Update weight_methods.py

--- utils/weight_methods.py
+++ utils/weight_methods.py
@@ -531,6 +531,79 @@ class ExcessMTL(WeightMethod):
 
         return loss, extra_outputs
 
+class FAMO(WeightMethod):
+    """
+    Fast Adaptive Multitask Optimization.
+    https://github.com/Cranial-XIX/FAMO/blob/main/famo.py
+    """
+    def __init__(
+        self,
+        n_tasks: int,
+        device: torch.device,
+        gamma: float = 0.01,   # the regularization coefficient
+        w_lr: float = 0.001,   # the learning rate of the task logits
+        max_norm: float = 1.0, # the maximum gradient norm
+    ):
+        super().__init__(
+            n_tasks=n_tasks, 
+            device=device)
+                
+        self.min_losses = torch.zeros(n_tasks).to(device)
+        self.w = torch.tensor([0.0] * n_tasks, device=device, requires_grad=True)
+        self.w_opt = torch.optim.Adam([self.w], lr=w_lr, weight_decay=gamma)
+        self.max_norm = max_norm
+    
+    def set_min_losses(self, losses):
+        self.min_losses = losses
+
+    def get_weighted_loss(self, losses):
+        extra_outputs = dict()
+        self.prev_loss = torch.stack(losses)
+        z = F.softmax(self.w, -1)
+        D = torch.stack(losses) - self.min_losses + 1e-8
+        c = (z / D).sum().detach()
+        loss = (D.log() * z / c).sum()
+        extra_outputs["weights"] = c
+        return loss, extra_outputs
+
+    def update(self, curr_loss):
+        delta = (self.prev_loss - self.min_losses + 1e-8).log() - \
+                (torch.stack(curr_loss)      - self.min_losses + 1e-8).log()
+        with torch.enable_grad():
+            d = torch.autograd.grad(F.softmax(self.w, -1),
+                                    self.w,
+                                    grad_outputs=delta.detach())[0]
+        self.w_opt.zero_grad()
+        self.w.grad = d
+        self.w_opt.step()
+
+    def backward(
+        self,
+        losses: torch.Tensor,
+        shared_parameters: Union[
+            List[torch.nn.parameter.Parameter], torch.Tensor
+        ] = None,
+        task_specific_parameters: Union[
+            List[torch.nn.parameter.Parameter], torch.Tensor
+        ] = None,
+    ) -> Union[torch.Tensor, None]:
+        """
+        Parameters
+        ----------
+        losses :
+        shared_parameters :
+        last_shared_parameters : parameters of last shared layer/block
+        Returns
+        ----------
+        Loss, extra outputs
+        """
+        loss, extra_outputs = self.get_weighted_loss(losses=losses)
+        loss.backward()
+        if self.max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(shared_parameters, self.max_norm)
+        return loss, extra_outputs
+
+
 
 class LinearScalarization(WeightMethod):
     """Linear scalarization baseline L = sum_j w_j * l_j where l_j is the loss for task j and w_h"""
